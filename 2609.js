2f65c51da75001b8ed6cbf22843b117af45ec833
mj-song00 <niveus128@gmail.com>
2023-04-18 09:16:15 +0900

    입력값 수정  및 문제 풀이 주석 추가

--- 2609.js
+++ 2609.js
@@ -1,13 +1,18 @@
 // 두 개의 자연수를 입력받아 최대 공약수와 최소 공배수를 출력하는 프로그램을 작성하시오.
 
-// 받은 두 수를 소수가 될때까지 나눈다
+//18 = 2 * 3 * 3
 // 24 = 2 * 2 * 2 * 3
-// 18 = 2 * 3 * 3
+
 const [a, b] = require("fs")
   .readFileSync("2609.txt")
   .toString()
-  .split("\n")
-  .map((i) => parseInt(i));
+  .trim() //1. 공백제거 후 새로운 문자열 반환
+  .split(" ")
+  .map((i) => parseInt(i)); //2. 구조분해 할당 사용 3. 문자를 숫자로 변경
+
+// let a, b, rest; // 변수선언
+// [a, b] = [10, 20];
+// console.log(a);
 
 let i = a;
 let j = b;
@@ -18,6 +23,7 @@ while (i % j !== 0) {
     i = j;
     j = n;
   }
-}
-console.log(j);
-console.log((i * j) / j);
+} //조건이 ture일경우 계속 반복
+
+console.log(j); // 최대공약수
+console.log((a * b) / j); //최소공배수
