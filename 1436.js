07ac00ffebd391cc4ef2edab5fc48f4db7926280
mj-song00 <niveus128@gmail.com>
2023-07-03 11:50:18 +0900

    23.07.03

---
+++ 1436.js
@@ -0,0 +1,14 @@
+const input = require("fs").readFileSync("1436.txt").toString().trim();
+
+let a = 666;
+
+while (true) {
+  if (a.toString().includes("666")) {
+    input -= 1;
+    if (input == 0) break;
+  }
+
+  a += 1;
+}
+
+console.log(a);
