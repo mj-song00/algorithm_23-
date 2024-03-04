2c57ead6ce285a293372828d4208b49970c9fbb2
mj-song00 <niveus128@gmail.com>
2024-03-04 16:46:45 +0900

    24.03.04

---
+++ 1076.js
@@ -0,0 +1,20 @@
+let input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const table = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+};
+
+console.log((table[input[0]] * 10 + table[input[1]]) * 10 ** table[input[2]]);
